103a04278aab02069929d9adce3725f55a7282d8
poonam <gusinpoonam5@gmail.com>
2024-09-26 17:36:25 +0530

    commit in new branch

--- src/G1/Class1.java
+++ src/G1/Class1.java
@@ -5,7 +5,7 @@ public class Class1 {
     public static void main(String[] args) {
 		int a=5,b=6,sum=0;
 		sum=a+b;
-		System.out.println("The sum of 2 numbers is"+sum);
+		System.out.println("The sum of 2 numbersdfdsfgg is"+sum);
 		
 		
 

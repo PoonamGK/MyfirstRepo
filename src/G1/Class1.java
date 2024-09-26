fba3901e428829c340fe3029e330efcdd8586faf
poonam <gusinpoonam5@gmail.com>
2024-09-26 18:17:10 +0530

    commit in kan127

--- src/G1/Class1.java
+++ src/G1/Class1.java
@@ -5,7 +5,7 @@ public class Class1 {
     public static void main(String[] args) {
 		int a=5,b=6,sum=0;
 		sum=a+b;
-		System.out.println("The sum of 2 aaaanumbersdgjgjgjgjfdsfgg is"+sum);
+		System.out.println("The sum of ttttt aaaanumbersdgjgjgjgjfdsfgg is"+sum);
 		
 		
 

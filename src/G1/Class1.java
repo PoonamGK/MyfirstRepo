c0c2561f6e450a91170cd6443f4e62acce8e8d9d
poonam <poonam.kandari07@gmail.com>
2024-09-26 14:27:28 +0530

    My first commit

---
+++ src/G1/Class1.java
@@ -0,0 +1,14 @@
+package G1;
+
+public class Class1 {
+	
+    public static void main(String[] args) {
+		int a=5,b=6,sum=0;
+		sum=a+b;
+		System.out.println("The sum of 2 numbers is"+sum);
+		
+		
+
+	}
+
+}
